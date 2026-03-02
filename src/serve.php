e577b7786992687e0304c77a314e517d6ba7e93a
nycki <nupanick@gmail.com>
2026-03-02 08:19:44 -0800

    consistent include path

--- src/serve.php
+++ src/serve.php
@@ -1,3 +1,3 @@
 <?php 
-putenv('include_path='.__DIR__.PATH_SEPARATOR.__DIR__.'/../include');
+putenv('include_path='.__DIR__.'/../include'.PATH_SEPARATOR.__DIR__);
 exec('php -S localhost:8000 -t public -c php.ini');
